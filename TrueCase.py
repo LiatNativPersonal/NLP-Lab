92f97f64338aa6124539f0f0e0caeefdb6869def
LiatNativ <liatnativ@gmail.com>
2018-06-27 14:52:12 +0300

    Added code to prevent performing true case if data already exist

--- TrueCase.py
+++ TrueCase.py
@@ -4,54 +4,70 @@ import numpy as np
 
 
 def correctCase(trigrams_file, unigrams_file, native_directory_path, non_native_directory_path, nonNativeOutputFileName, nativeOutputFileName):
-    trigramsDB = open(trigrams_file,"r")
-    unigramsDB = open(unigrams_file,"r")
-    trigramDictionary = createTrigramDictionary(trigramsDB)
-    unigramDictionary = createUnigramDictionary(unigramsDB)
-    nonNativeOutputFile = open(nonNativeOutputFileName,"w", encoding="utf-8")
-    nativeOutputFile = open(nativeOutputFileName,"w", encoding="utf-8")
-    nonNativeSentenceArray = []
-    nonNativeNewSentenceArray = []
-    nativeSentenceArray = []
-    nativeNewSentenceArray = []
-       
-    nativeFileList = os.listdir(native_directory_path)
-    nonNativeFileList = os.listdir(non_native_directory_path)
+   
+   
     
-    # NON NATIVE
-    for file in nonNativeFileList:       
-        nonNativeFile = open(non_native_directory_path + "\\" + file,"r", encoding = "utf8",  errors="ignore")
-        print("Running file " + str(file))
-        nonNativeSentenceArray += (createSentencesArray(nonNativeFile))
-        nonNativeFile.close()
-
-    for sentence in nonNativeSentenceArray:
-        sentence = removeBrackets(sentence)
-        sentence = handleTitle(sentence)
-        sentence = handleUpperLowerCase(sentence, trigramDictionary, unigramDictionary)
-        nonNativeNewSentenceArray.append(sentence)
-    for sentence in nonNativeNewSentenceArray:
-            nonNativeOutputFile.write(sentence + "\n")     
-    nonNativeOutputFile.close()
-    
-    # NATIVE
-    for file in nativeFileList:       
-        nativeFile = open(native_directory_path + "\\" + file, "r", encoding = "utf8",  errors = "ignore")
-        print("Running file " + str(file))
-        nativeSentenceArray += (createSentencesArray(nativeFile))
-        nativeFile.close()
+    if not os.path.exists(nonNativeOutputFileName) or not os.path.exists(nativeOutputFileName):
+        print("Need to perform TrueCasing") 
+        trigramsDB = open(trigrams_file,"r")
+        unigramsDB = open(unigrams_file,"r")
+        trigramDictionary = createTrigramDictionary(trigramsDB)
+        unigramDictionary = createUnigramDictionary(unigramsDB)
+        # NON NATIVE              
+        if not os.path.exists(nonNativeOutputFileName):
+            print("True-casing non-nativ data")
+            nonNativeOutputFile = open(nonNativeOutputFileName,"w", encoding="utf-8")    
+            nonNativeSentenceArray = []
+            nonNativeNewSentenceArray = []
+            nonNativeFileList = os.listdir(non_native_directory_path)   
+            
+            for file in nonNativeFileList:       
+                nonNativeFile = open(non_native_directory_path + "\\" + file,"r", encoding = "utf8",  errors="ignore")
+                print("Running file " + str(file))
+                nonNativeSentenceArray += (createSentencesArray(nonNativeFile))
+                nonNativeFile.close()
         
-    for sentence in nativeSentenceArray:
-        sentence = removeBrackets(sentence)
-        sentence = handleTitle(sentence)
-        sentence = handleUpperLowerCase(sentence, trigramDictionary, unigramDictionary)
-        nativeNewSentenceArray.append(sentence)
-    for sentence in nativeNewSentenceArray:
-            nativeOutputFile.write(sentence + "\n")     
-    nativeOutputFile.close()
-      
-    trigramsDB.close()
-    unigramsDB.close()
+            for sentence in nonNativeSentenceArray:
+                sentence = removeBrackets(sentence)
+                sentence = handleTitle(sentence)
+                sentence = handleUpperLowerCase(sentence, trigramDictionary, unigramDictionary)
+                nonNativeNewSentenceArray.append(sentence)
+            for sentence in nonNativeNewSentenceArray:
+                    nonNativeOutputFile.write(sentence + "\n")     
+            nonNativeOutputFile.close()
+        else:
+            print("Truecase phase for non-native skipped - data exist")
+         # NATIVE   
+        if not os.path.exists(nativeOutputFileName):
+            print("True-casing nativ data")
+            nativeOutputFile = open(nativeOutputFileName,"w", encoding="utf-8")
+            nativeSentenceArray = []
+            nativeNewSentenceArray = []       
+            nativeFileList = os.listdir(native_directory_path)
+            
+            for file in nativeFileList:       
+                nativeFile = open(native_directory_path + "\\" + file, "r", encoding = "utf8",  errors = "ignore")
+                print("Running file " + str(file))
+                nativeSentenceArray += (createSentencesArray(nativeFile))
+                nativeFile.close()
+                
+            for sentence in nativeSentenceArray:
+                sentence = removeBrackets(sentence)
+                sentence = handleTitle(sentence)
+                sentence = handleUpperLowerCase(sentence, trigramDictionary, unigramDictionary)
+                nativeNewSentenceArray.append(sentence)
+            for sentence in nativeNewSentenceArray:
+                    nativeOutputFile.write(sentence + "\n")     
+            nativeOutputFile.close()
+        else:
+            print("Truecase phase for native skipped - data exist")
+          
+        trigramsDB.close()
+        unigramsDB.close()
+    else:
+        print("Truecase phase  skipped - data exist")
+        
+   
 
 def createSentencesArray(file):
     sentenceArry = []
